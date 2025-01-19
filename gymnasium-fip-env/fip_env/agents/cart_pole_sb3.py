08334d9246610bb652db3ad7bb508065f4b312d1
Sergey Royz <zjor.se@gmail.com>
2025-01-19 15:12:41 +0100

    continuous force applied to stabilize the pendulum

--- gymnasium-fip-env/fip_env/agents/cart_pole_sb3.py
+++ gymnasium-fip-env/fip_env/agents/cart_pole_sb3.py
@@ -1,11 +1,16 @@
 """
+CartPole-v1 source code:
+    https://github.com/Farama-Foundation/Gymnasium/blob/main/gymnasium/envs/classic_control/cartpole.py
+
 How to run
     python -m fip_env.agents.cart_pole_sb3 --train
 """
 from typing import Any
 
 import gymnasium as gym
-from gymnasium import Env
+import numpy as np
+from gymnasium import Env, spaces, logger
+
 from stable_baselines3 import PPO
 from stable_baselines3.common.base_class import BaseAlgorithm
 from stable_baselines3.common.callbacks import StopTrainingOnRewardThreshold, EvalCallback
@@ -15,24 +20,99 @@ ENV_ID = "CartPole-v1"
 
 
 class CenteredCartPoleEnv(gym.Env):
-    def __init__(self, **kwargs):
+    def __init__(self,
+                 action_threshold=10.0,
+                 kick_probability=0.0,
+                 kick_strength=1.0,
+                 max_steps=500,
+                 theta_threshold_radians=None, **kwargs):
         self.env = gym.make(ENV_ID, **kwargs)  # Original CartPole environment
-        self.action_space = self.env.action_space
+        self._super_env = self.env.unwrapped
+        self.theta_threshold_radians = self._super_env.theta_threshold_radians if theta_threshold_radians is None else theta_threshold_radians
+
+        # self.action_space = self.env.action_space
         self.observation_space = self.env.observation_space
-        self.max_steps = 500  # Max steps per episode (same as CartPole-v1)
+        self.action_space = spaces.Box(low=-action_threshold, high=action_threshold, shape=(1,), dtype=np.float32)
+        self.max_steps = max_steps
         self.current_step = 0
+
+        self.kick_probability = kick_probability
+        self.kick_strength = kick_strength
 
     def reset(self, **kwargs):
         self.current_step = 0
         return self.env.reset(**kwargs)
 
+    def _super_step(self, action):
+        _super_env = self.env.unwrapped
+        x, x_dot, theta, theta_dot = _super_env.state
+        # force = _super_env.force_mag if action == 1 else -_super_env.force_mag
+        force = np.clip(action[0], self.action_space.low[0], self.action_space.high[0])  # apply continuous force
+        costheta = np.cos(theta)
+        sintheta = np.sin(theta)
+
+        # For the interested reader:
+        # https://coneural.org/florian/papers/05_cart_pole.pdf
+        temp = (
+                       force + _super_env.polemass_length * np.square(theta_dot) * sintheta
+               ) / _super_env.total_mass
+        thetaacc = (_super_env.gravity * sintheta - costheta * temp) / (
+                _super_env.length
+                * (4.0 / 3.0 - _super_env.masspole * np.square(costheta) / _super_env.total_mass)
+        )
+        xacc = temp - _super_env.polemass_length * thetaacc * costheta / _super_env.total_mass
+
+        if _super_env.kinematics_integrator == "euler":
+            x = x + _super_env.tau * x_dot
+            x_dot = x_dot + _super_env.tau * xacc
+            theta = theta + _super_env.tau * theta_dot
+            theta_dot = theta_dot + _super_env.tau * thetaacc
+        else:  # semi-implicit euler
+            x_dot = x_dot + _super_env.tau * xacc
+            x = x + _super_env.tau * x_dot
+            theta_dot = theta_dot + _super_env.tau * thetaacc
+            theta = theta + _super_env.tau * theta_dot
+
+        _super_env.state = np.array((x, x_dot, theta, theta_dot), dtype=np.float64)
+
+        terminated = bool(
+            x < -_super_env.x_threshold
+            or x > _super_env.x_threshold
+            or theta < -self.theta_threshold_radians
+            or theta > self.theta_threshold_radians
+        )
+
+        if not terminated:
+            reward = 0.0 if _super_env._sutton_barto_reward else 1.0
+        elif _super_env.steps_beyond_terminated is None:
+            # Pole just fell!
+            _super_env.steps_beyond_terminated = 0
+
+            reward = -1.0 if _super_env._sutton_barto_reward else 1.0
+        else:
+            if _super_env.steps_beyond_terminated == 0:
+                logger.warn(
+                    "You are calling 'step()' even though this environment has already returned terminated = True. "
+                    "You should always call 'reset()' once you receive 'terminated = True' -- any further steps are undefined behavior."
+                )
+            _super_env.steps_beyond_terminated += 1
+
+            reward = -1.0 if _super_env._sutton_barto_reward else 0.0
+
+        if _super_env.render_mode == "human":
+            _super_env.render()
+
+        # truncation=False as the time limit is handled by the `TimeLimit` wrapper added during `make`
+        return np.array(_super_env.state, dtype=np.float32), reward, terminated, False, {}
+
     def step(self, action):
-        obs, reward, terminated, truncated, info = self.env.step(action)
+        obs, reward, terminated, truncated, info = self._super_step(action)
 
-        # Extract cart position (first element in the observation)
+        if np.random.random() < self.kick_probability:
+            # apply kick to the pole's angular velocity
+            obs[3] += np.random.uniform(-self.kick_strength, self.kick_strength)
+
         cart_position = obs[0]
-
-        # Penalize the cart for being away from the center
         position_penalty = -abs(cart_position)  # Negative reward for being away from center
 
         # Combine the original reward with the position penalty
@@ -91,7 +171,7 @@ def main(should_train: bool = True):
 
         _train(env, model)
 
-        eval_env = gym.make(ENV_ID)
+        eval_env = _get_env()
         # Evaluate the model
         mean_reward, std_reward = evaluate_policy(model, eval_env, n_eval_episodes=10)
         print(f"Mean reward: {mean_reward}, Std reward: {std_reward}")
@@ -99,7 +179,7 @@ def main(should_train: bool = True):
         model.save(filename)
     else:
         model = PPO.load(filename)
-        env = _get_env(render_mode="human")
+        env = _get_env(render_mode="human", kick_probability=0.5, kick_strength=2.0, max_steps=1000, theta_threshold_radians=24 * 2 * np.pi / 360)
         obs, _ = env.reset()
         for _ in range(1000):
             action, _states = model.predict(obs, deterministic=True)
@@ -117,3 +197,5 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     main(should_train=args.train)
+    # env = _get_env()
+    # print(env.action_space.sample())
